4df0d365eabd11d09d75e46d1810281e7729a593
Josh Bright <josh.bright@gmail.com>
2024-12-01 12:32:42 -0800

    Added main script for game

---
+++ finns-quest/scripts/Main.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var is_new_game = true
+var current_map_name = null
+var start_map_name = "World"
+
+@onready var world_manager = $WorldManager
+@onready var player = $Player
+
+func _ready():
+	world_manager.load_map(start_map_name)
+	is_new_game = false
+
+func _on_world_manager_map_loaded(map_name: Variant) -> void:
+	print("Map Loaded: ", map_name)
+	var player_pos = null
+	if is_new_game:
+		player_pos = world_manager.get_marker_position("GameStart")
+	else:
+		# find the position based on our last map
+		player_pos = world_manager.get_marker_position("From" + current_map_name)
+	
+	# we can now change the current map name to our newly loaded map
+	current_map_name = map_name
+	
+	# position player
+	player.position = player_pos
